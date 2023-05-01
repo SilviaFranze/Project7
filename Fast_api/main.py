7472414570e8742445af8f6c055784d310efaab7
silviafranze <silvia.franze.521@gmail.com>
2023-05-01 16:43:08 +0200

    prediction function working locally

--- Fast_api/main.py
+++ Fast_api/main.py
@@ -7,21 +7,27 @@ from pydantic import BaseModel
 from lightgbm import LGBMClassifier
 
 app = FastAPI()
-input_data_scaled = joblib.load("../Data&output/test_scaled_data_ok.joblib")
-light_classif = joblib.load("../Data&output/lightgbmodel.joblib")
+
+#Load customer data
+input_data_scaled = joblib.load("../Data&output/X_tst_sld_skid.joblib")
+# Load the LightGBM model
+lgbm_classif = joblib.load("../Data&output/lightgbmodel.joblib")
 
 
-# utiliser le get pour choisir l id
 
 @app.get('/prediction/{id_client}')
 
 
-def predizione_lightgbm(numero):
-    # loads the trained LightGBM model
-    model = lgb.Booster(model_file='modello_lgb.txt')
-    
+def prediction_client(id_client: int):   
+    '''
+    Endpoint to get the client id and return the prediction based on a pre trained LightGBM model
+    '''    
+
+    # Select customer data specified by ID and dropping the ID column
+    selected_customer = input_data_scaled[input_data_scaled['SK_ID_CURR'] == id_client].drop('SK_ID_CURR', axis=1)
+
     # makes the prediction on the index given as input
-    predizione = model.predict([numero])[0]
+    predizione = lgbm_classif.predict_proba(selected_customer)[:,0][0]
     
     # determines whether the application was accepted or rejected on the basis of the 0.90 threshold
     if predizione > 0.90:
@@ -29,13 +35,9 @@ def predizione_lightgbm(numero):
     else:
         decision = "refused"
 
-
-def read_id(id_client: int):
-	"""
-	Endpoint to	"""
-
-	return {'message': id_client,
-			'decision': 'accordé'}    #
+    # returns a dictionary with the client ID and the decision made
+    return {"Customer id": id_client, 
+            "Decision": decision}
 
 
 if __name__ == '__main__':
