c4158d0a26de36ff6339e8e81149c7eadfadb5ae
silviafranze <silvia.franze.521@gmail.com>
2023-05-01 15:59:33 +0200

    towards implementation of prediction function in main.py

--- Fast_api/main.py
+++ Fast_api/main.py
@@ -14,13 +14,28 @@ light_classif = joblib.load("../Data&output/lightgbmodel.joblib")
 # utiliser le get pour choisir l id
 
 @app.get('/prediction/{id_client}')
+
+
+def predizione_lightgbm(numero):
+    # loads the trained LightGBM model
+    model = lgb.Booster(model_file='modello_lgb.txt')
+    
+    # makes the prediction on the index given as input
+    predizione = model.predict([numero])[0]
+    
+    # determines whether the application was accepted or rejected on the basis of the 0.90 threshold
+    if predizione > 0.90:
+        decision = "accepted"
+    else:
+        decision = "refused"
+
+
 def read_id(id_client: int):
 	"""
 	Endpoint to	"""
-	client = input_data_scaled.loc[input_data_scaled['SK_ID_CURR'] == {id_client}].iloc[:,1:]
-	pd.DataFrame(light_classif.predict_proba(client))
+
 	return {'message': id_client,
-			'decision': 'accordé'}
+			'decision': 'accordé'}    #
 
 
 if __name__ == '__main__':
@@ -28,3 +43,11 @@ if __name__ == '__main__':
 
 # uvicorn main:app --reload
 
+
+
+"""
+a voir ou rajouter ce partie la dans le get de la prediction
+
+	client = input_data_scaled.loc[input_data_scaled['SK_ID_CURR'] == {id_client}].iloc[:,1:]
+	pd.DataFrame(light_classif.predict_proba(client))
+"""
