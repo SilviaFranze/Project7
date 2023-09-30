8541de5a1eb25f5c8b089a9f23e58e5e54877769
silviafranze <silvia.franze.521@gmail.com>
2023-09-30 13:17:55 +0200

    update unit test - correction

--- test_client_id.py
+++ test_client_id.py
@@ -1,20 +1,21 @@
 import requests
 
-BASE_URL = "http://slviafranze.pythonanywhere.com"
+BASE_URL = "http://silviafranze.pythonanywhere.com"
 
 def test_valid_client_id():
     client_id = 126503  # Example valid client ID that exists in your dataset
     response = requests.get(f"{BASE_URL}/prediction/{client_id}")
     
+    
     # Check if the response status code is 200 (OK)
     assert response.status_code == 200
-    
+
 def test_invalid_client_id_format():
     client_id = 6549871  # Example invalid client ID format
     response = requests.get(f"{BASE_URL}/prediction/{client_id}")
     
-    # Check if the response status code is 400 (Bad Request) or another appropriate error code
-    assert response.status_code == 400
+    # Check if the response status code is 500, the one I set
+    assert response.status_code == 500
 
 
 """ def test_invalid_client_id_not_in_dataset():
