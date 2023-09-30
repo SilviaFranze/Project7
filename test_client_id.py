cf73f0a2ba6b57af661de0a0d5253234083a5c22
silviafranze <silvia.franze.521@gmail.com>
2023-09-30 12:55:41 +0200

    little corrections

--- test_client_id.py
+++ test_client_id.py
@@ -3,14 +3,14 @@ import requests
 BASE_URL = "http://slviafranze.pythonanywhere.com"
 
 def test_valid_client_id():
-    client_id = 123456  # Example valid client ID that exists in your dataset
+    client_id = 126503  # Example valid client ID that exists in your dataset
     response = requests.get(f"{BASE_URL}/prediction/{client_id}")
     
     # Check if the response status code is 200 (OK)
     assert response.status_code == 200
     
 def test_invalid_client_id_format():
-    client_id = "invalid_id"  # Example invalid client ID format
+    client_id = 6549871  # Example invalid client ID format
     response = requests.get(f"{BASE_URL}/prediction/{client_id}")
     
     # Check if the response status code is 400 (Bad Request) or another appropriate error code
