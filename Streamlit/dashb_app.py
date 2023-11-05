af4bf979a3eeae07e679757ebc31b6bd356a79c2
silviafranze <silvia.franze.521@gmail.com>
2023-11-05 17:41:00 +0100

    up3kòòklj

--- Streamlit/dashb_app.py
+++ Streamlit/dashb_app.py
@@ -14,45 +14,11 @@ st.write("Files in current directory:", os.listdir('.'))
 #input_data = joblib.load("project7/Streamlit/data4streamlit.joblib")     # /home/silviafranze pour le run sur python anywhere  # substitute them with the actual functioning dataset, to calculate the explainer etc
 #client_ids =  joblib.load("/mount/src/project7/Streamlit/list_id_clients_long.joblib")
 # URL del file raw su GitHub
-url = "https://github.com/SilviaFranze/Project7/raw/master/Streamlit/list_id_clients_long.joblib"
+#url = "https://github.com/SilviaFranze/Project7/raw/master/Streamlit/list_id_clients_long.joblib"
 
-# Effettua una richiesta GET per scaricare il file
-response = requests.get(url)
+input_data = joblib.load('project7/Streamlit/data4streamlit.joblib')
 
-# Verifica che la richiesta sia andata a buon fine
-response.raise_for_status()
-
-# Crea un oggetto file-like dai dati raw
-file_like_object = io.BytesIO(response.content)
-
-# Carica i dati joblib dall'oggetto file-like
-input_data = joblib.load(file_like_object)
-
-
-"""
-# Token di Accesso Personale (recuperato dai Secrets di Streamlit)
-pat = st.secrets["DB_USERNAME"]
-pat2 = st.secrets["DB_TOKEN"]
-
-# Imposta l'autenticazione per la richiesta
-headers = {'Authorization': f'token {pat}'}
-
-# Scarica i dati raw dai file su GitHub
-response_data = requests.get(url_data, headers=headers)
-response_data.raise_for_status()  # Verifica che la richiesta sia andata a buon fine
-
-response_ids = requests.get(url_ids, headers=headers)
-response_ids.raise_for_status()  # Verifica che la richiesta sia andata a buon fine
-
-# Crea oggetti file-like dai dati raw
-file_like_object_data = io.BytesIO(response_data.content)
-file_like_object_ids = io.BytesIO(response_ids.content)
-
-# Carica i dati joblib dagli oggetti file-like
-input_data = joblib.load(file_like_object_data)
-client_ids = joblib.load(file_like_object_ids)
-
-"""
+explainer = st_shap.TreeExplainer(light_for_shap, model_output='raw')
 
 st.title("Scoring prediction")
 st.write('Select the customer\'s ID to make a prediction on their loan request.')
