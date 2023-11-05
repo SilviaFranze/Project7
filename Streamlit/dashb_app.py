c77789b1f330a5b7875ffd8bf232f8ac346899b5
silviafranze <silvia.franze.521@gmail.com>
2023-11-05 17:24:55 +0100

    up3kòòkljòoj

--- Streamlit/dashb_app.py
+++ Streamlit/dashb_app.py
@@ -11,7 +11,7 @@ import os
 st.write("CIAoooooooooooo, Current directory:", os.getcwd())
 st.write("Files in current directory:", os.listdir('.'))
 
-input_data = joblib.load("/mount/src/project7/Streamlit/data4streamlit.joblib")     # /home/silviafranze pour le run sur python anywhere  # substitute them with the actual functioning dataset, to calculate the explainer etc
+input_data = joblib.load("project7/Streamlit/data4streamlit.joblib")     # /home/silviafranze pour le run sur python anywhere  # substitute them with the actual functioning dataset, to calculate the explainer etc
 client_ids =  joblib.load("/mount/src/project7/Streamlit/list_id_clients_long.joblib")
 
 """
