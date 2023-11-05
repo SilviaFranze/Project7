4ce50362c19af773f432336a7b252f61ba09e606
silviafranze <silvia.franze.521@gmail.com>
2023-11-05 19:14:03 +0100

    correcting single quotes

--- Streamlit/dashb_app.py
+++ Streamlit/dashb_app.py
@@ -16,7 +16,7 @@ st.write("Files in current directory:", os.listdir('.'))
 # URL del file raw su GitHub
 #url = "https://github.com/SilviaFranze/Project7/raw/master/Streamlit/list_id_clients_long.joblib"
 
-input_data = joblib.load('Streamlit/data4streamlit.joblib')
+input_data = joblib.load("Streamlit/data4streamlit.joblib")
 client_ids =  joblib.load("Streamlit/list_id_clients_long.joblib")
 explainer = joblib.load("Streamlit/shap_explainer.joblib")
 
